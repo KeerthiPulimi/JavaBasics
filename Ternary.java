752fa8c52d0f3d2930f9fca44109a56f7a7b0750
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-01-23 18:37:06 +0530

    ternary operation

---
+++ Ternary.java
@@ -0,0 +1,8 @@
+public class Ternary {
+  public static void main(String a[]) {
+    int n = 5;
+    int result = 0;
+    result = n % 2 == 0 ? 10 : 20;
+    System.out.println(result); // Print the result
+  }
+}
