cf2b7c52aec25322c8b0daa908820d87e20bf292
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-02-08 17:36:23 +0530

    jagged array

--- MulArrayExample.java
+++ MulArrayExample.java
@@ -1,35 +1,35 @@
 public class MulArrayExample {
   public static void main(String[] args) {
-    int num[][] = new int[3][4];
+    int num[][] = new int[3][];
+    num[0] = new int[3];
+    num[1] = new int[4];
+    num[2] = new int[5];
 
     // Fill the array with random values (0 to 99)
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 4; j++) {
-        num[i][j] = (int) (Math.random() * 100); // Corrected
-        // System.out.print(num[i][j] + " ");
+    for (int i = 0; i < num.length; i++) {
+      for (int j = 0; j < num[i].length; j++) { // ✅ Corrected
+        num[i][j] = (int) (Math.random() * 10);
       }
-      System.out.println();
     }
 
     System.out.println("------ Printing Again ------");
 
     // Print the array again
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < num.length; i++) { // ✅ Use num.length
+      for (int j = 0; j < num[i].length; j++) { // ✅ Use num[i].length
         System.out.print(num[i][j] + " ");
       }
       System.out.println();
     }
 
-    System.out.println("------ ENHANCED ARRAY ------");
+    System.out.println("------ ENHANCED FOR LOOP ------");
 
-    // enhanced for loop
-    for (int n[] : num) {
-      for (int m : n) {
-        System.out.print(m + " ");
+    // Enhanced for loop
+    for (int[] row : num) {
+      for (int value : row) {
+        System.out.print(value + " ");
       }
       System.out.println();
-
     }
   }
 }
