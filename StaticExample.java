db63f5a6d8e95e9fa65d18a5e318084e48191427
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-02-12 15:34:32 +0530

    added the static

--- StaticExample.java
+++ StaticExample.java
@@ -2,7 +2,20 @@ class Mobile {
   String brand;
   int price;
 
-  String name;
+  static String name;
+
+  public Mobile() {
+    brand = "";
+    price = 200;
+    // name="phone";
+    System.out.println("constructor");
+
+  }
+
+  static {
+    name = "phone";
+    System.out.println("statis");
+  }
 
   public void show() {
     System.out.println(brand + ":" + price + ":" + name);
@@ -17,16 +30,26 @@ public class StaticExample {
     Mobile obj1 = new Mobile();
     obj1.brand = "vivo";
     obj1.price = 22000;
-    obj1.name = "smartphone";
+    // obj1.name = "smartphone";
+    Mobile.name = "smartphone";
 
-    Mobile obj2 = new Mobile();
-    obj2.brand = "apple";
-    obj2.price = 222000;
-    obj2.name = "ios";
+    // // Mobile obj2 = new Mobile();
+    // // obj2.brand = "apple";
+    // // obj2.price = 222000;
+    // // // obj2.name = "ios";
+    // // // Mobile.name = "ios";
 
-    obj1.show();
-    obj2.show();
+    // // obj1.show();
+    // // obj2.show();
+
+    Mobile obj3 = new Mobile();
 
   }
 
 }
+
+// when every object is being created the class loader loads the class and
+// static is called and after that 2 times the constructor is called ,,,,, if
+// you dont create any object no output will come ,,,, but what if you want to
+// load the class into class loader but not create any object ,, in that caase
+// you can use class class
