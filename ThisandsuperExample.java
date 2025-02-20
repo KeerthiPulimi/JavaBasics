59e50de6a8a2dbcfaec67142c1dd541e30569f41
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-02-20 17:47:55 +0530

    this and super keyword

---
+++ ThisandsuperExample.java
@@ -0,0 +1,39 @@
+class A {
+  public A() {
+    System.out.println("in A");
+  }
+
+  public A(int n) {
+    super();
+    System.out.println("in  int A");
+  }
+
+}
+
+class B extends A {
+  public B() {
+    super();
+    System.out.println("in B");
+  }
+
+  public B(int n) {
+    this();
+    System.out.println("in int  B");
+  }
+
+}
+
+public class ThisandsuperExample {
+  public static void main(String[] args) {
+    B obj = new B(5);
+  }
+
+}
+
+// super method means call the constructor of super class
+
+// every class in java extends object class
+
+// for B ,A is super class, and for A , Object is super class
+
+// this keyword will execte the constructor for same class
