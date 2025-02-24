579d3406409f373d9b10f513f47ca3859a0e3d3a
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-02-24 18:16:26 +0530

    added about packages

---
+++ PackageExample.java
@@ -0,0 +1,11 @@
+import tools.Calc;
+
+public class PackageExample {
+
+  Calc obj = new Calc();
+
+}
+
+// you need to import a package
+// using * imports every thing
+// * imports the files not folders
