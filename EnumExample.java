7e733ec384ed728a388718f7d531bd64a58b1e92
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-02-28 22:06:13 +0530

    enum with if-else and switch

--- EnumExample.java
+++ EnumExample.java
@@ -5,17 +5,40 @@ enum Status {
 
 public class EnumExample {
   public static void main(String[] args) {
-    Status s = Status.Running;
-    System.out.println(s);
-    System.out.println(s.ordinal());
-    Status ss[] = Status.values();
-    System.out.println(ss[0]);
-    for (Status sss : ss) {
-      System.out.println(sss + ":" + sss.ordinal());
+    Status s = Status.Pending;
+    // System.out.println(s);
+    // System.out.println(s.ordinal());
+    // Status ss[] = Status.values();
+    // System.out.println(ss[0]);
+    // for (Status sss : ss) {
+    // System.out.println(sss + ":" + sss.ordinal());
+
+    // }
+
+    // Status a = Status.Pending;
+
+    switch (s) {
+      case Running:
+        System.out.println("all good");
+        break;
+
+      case Pending:
+        System.out.println("pending");
+        break;
+      default:
+        System.out.println("bye");
 
     }
 
-    Status a = Status.Pending;
+    if (s == Status.Running) {
+      System.out.println("all good");
+    } else if (s == Status.Failed) {
+      System.out.println("try again");
+    } else if (s == Status.Pending) {
+      System.out.println("please wait");
+    } else {
+      System.out.println("all done");
+    }
 
   }
 
