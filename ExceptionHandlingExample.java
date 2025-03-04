934a2bdd872ebb3cf714d5044b897ad8cdcff4b2
KeerthiPulimi <pulimikeerthi155@gmail.com>
2025-03-04 14:11:40 +0530

    throw keyword

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -1,23 +1,28 @@
 public class ExceptionHandlingExample {
   public static void main(String[] args) {
-    int i = 2;
+    int i = 20;
     int j = 0;
     int nums[] = new int[5];
     String str = null;
 
     try {
       j = 18 / i;
+      if (j == 0) {
+        throw new ArithmeticException("i dont want to print 0");
+      }
     } catch (ArithmeticException e) {
-      System.out.println("cannot divide by zero" + e);
+      j = 18 / i;
+      System.out.println("thats the default " + e);
     }
 
-    try {
-      System.out.println(str.length());
-      System.out.println(nums[1]); // Valid
-      System.out.println(nums[5]); // ❌ Still out of bounds
-    } catch (ArrayIndexOutOfBoundsException e1) {
-      System.out.println("out of limit");
-    } catch (Exception e2) {
+    // try {
+    // System.out.println(str.length());
+    // System.out.println(nums[1]); // Valid
+    // System.out.println(nums[5]); // ❌ Still out of bounds
+    // } catch (ArrayIndexOutOfBoundsException e1) {
+    // System.out.println("out of limit");
+    // }
+    catch (Exception e2) {
       System.out.println("something went wrong");
     }
 
